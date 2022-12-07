d29a736d73f14df7615d9e6b16b0fba15545f06e
SooHyun-Kang <k.josephine19@gmail.com>
2022-12-07 15:49:38 -0800

    add comments throughout the entire code

--- src/polybius.js
+++ src/polybius.js
@@ -1,10 +1,4 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
   const lookup = {
     a: 11,
     b: 21,
